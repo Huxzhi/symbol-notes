570a4f2e1fb0362005e81d342d931556b959cba7
huxzhi <huxzhi@gmail.com>
2026-06-01 14:49:39 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-import 'solid-devtools'
 import { render } from 'solid-js/web'
-import './index.css'
 import App from './App'
+import './index.css'
 
 render(() => <App />, document.getElementById('root')!)
